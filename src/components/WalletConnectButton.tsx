a59df73fb71fbd16084a0fef1f84969f657bcf63
ColourfulMelon <jordynnitch@gmail.com>
2025-02-13 16:26:07 +0200

    update WalletConnectButton.tsx

--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from "react"
-import { cn } from "@/lib/utils"
 import useMediaQuery from "@/hooks/use-media-query"
 import { Button } from "@/components/ui/button"
 import {
@@ -22,11 +21,10 @@ import {
     DrawerTitle,
     DrawerTrigger,
 } from "@/components/ui/drawer"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-
-
 import { useAccount, useConnect } from "@starknet-react/core"
+import Image from 'next/image';
+import loginImage from '@/public/loginImage.png';
+
 export default function WalletConnectButton() {
     const [open, setOpen] = React.useState<boolean>(false)
     
@@ -52,15 +50,27 @@ export function ProviderSelectDialog({ open, setOpen }: { open: boolean, setOpen
     if (isDesktop) {
         return (
             <Dialog open={open} onOpenChange={setOpen}>
-                <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>Connect Wallet</DialogTitle>
-                    </DialogHeader>
-                    {connectors.map((connector, index) => (
-                        <Button key={index} onClick={() => connect({ connector })}>
-                            Connect {connector.id}
-                        </Button>
-                    ))}
+                <DialogContent className="!rounded-3xl bg-primary max-h-[40rem] max-w-[80vw] w-fit">
+                    <div className="flex p-2">
+                        
+                        <div className="pr-8 w-[20rem]">
+                            <Image className="h-auto w-auto rounded-2xl" src={loginImage} alt="Login Image"/>
+                            
+                        </div>
+                        
+                        <div className="border border-white rounded-2xl bg-gradient-to-b from-white to-primary py-8 px-16">
+                            <div className="flex flex-col gap-4 justify-center h-full" >
+                                
+                                {connectors.map((connector, index) => (
+                                    <Button key={index} onClick={() => connect({ connector })}>
+                                        Connect {connector.id}
+                                    </Button>
+                                ))}
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
                 </DialogContent>
             </Dialog>
         )
@@ -68,15 +78,18 @@ export function ProviderSelectDialog({ open, setOpen }: { open: boolean, setOpen
     
     return (
         <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerContent>
+            <DrawerContent className="bg-gradient-to-t from-white to-primary px-16">
                 <DrawerHeader className="text-left">
-                    <DrawerTitle>Connect Wallet</DrawerTitle>
+                    <DrawerTitle className="text-center">Connect Wallet</DrawerTitle>
                 </DrawerHeader>
-                {connectors.map((connector) => (
-                    <button onClick={() => connect({ connector })}>
-                        Connect {connector.id}
-                    </button>
-                ))}
+                <div className=" flex flex-col gap-4 mb-8">
+                    {connectors.map((connector, index) => (
+                        <Button className="w-[20rem] mx-auto ring ring-ring" key={index} onClick={() => connect({ connector })}>
+                            Connect {connector.id}
+                        </Button>
+                    ))}
+                </div>
+                
                 <DrawerFooter className="pt-2">
                     <DrawerClose asChild>
                         <Button variant="outline">Cancel</Button>
