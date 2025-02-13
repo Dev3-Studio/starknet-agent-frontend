bd4d0d418fd43aa154ae536af9af28f629b9d21d
ColourfulMelon <jordynnitch@gmail.com>
2025-02-13 16:58:18 +0200

    add metadata files

---
+++ src/metadata.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// todo: update metadata
+export const SetMetadata = {
+    title: 'Title',
+    description: 'Description',
+    type: 'website',
+    siteName: 'SiteName',
+    url: process.env.SITE_URL,
+    author: 'Dev3.Studio',
+    author_url: 'https://dev3.studio',
+    themeColor: '#00FF00',
+    image: 'https://placehold.co/600x400',
+};
