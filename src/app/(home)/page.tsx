b2c3c99d7e1ff50eb9688053cffc3f446e9af877
ColourfulMelon <jordynnitch@gmail.com>
2025-02-13 15:18:31 +0200

    add usemediaquery home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,6 +9,7 @@ import AgentWithSuggestions from '@/AgentWithSuggestions';
 import HorizontalScroll from '@/HorizontalScroll';
 import left from '@/public/left.png';
 import right from '@/public/right.png';
+import useMediaQuery from '@/hooks/use-media-query';
 
 interface AgentCardProps {
     name: string;
@@ -26,23 +27,33 @@ const agents: AgentCardProps[] = [
 ]
 
 export default function Home() {
+    
+    // todo adjust breakpoint
+    const isMobile = useMediaQuery('(max-width: 640px)');
     return (
         <main className="w-full mx-auto space-y-8 overflow-x-hidden">
             {/* Hero Section */}
             <div className="px-10">
-                <section className="h-[25rem] bg-hero-gradient rounded-2xl flex max-w-custom mx-auto">
+                {!isMobile && <section className="h-[25rem] bg-hero-gradient rounded-2xl flex max-w-custom mx-auto">
+                    
                     <Image className="w-fit pl-8" src={left} alt="AI character images"/>
                     
                     <div className="flex flex-col justify-center h-full w-full max-w-2xl mx-auto px-4 z-10">
-                        <h1 className="text-center text-4xl mb-4 drop-shadow-2xl">Scale your professional workforce with <span className="font-bold">Ai Agents</span></h1>
+                        <h1 className="text-center text-4xl mb-4 drop-shadow-2xl">Scale your professional workforce
+                            with <span className="font-bold">Ai Agents</span></h1>
                         <div className="flex bg-muted rounded-xl py-1">
-                            <Input className="placeholder:text-foreground placeholder:font-light focus-visible:ring-0 border-0 my-1" placeholder="Search for any agent service.."/>
+                            <Input
+                                className="placeholder:text-foreground placeholder:font-light focus-visible:ring-0 border-0 my-1"
+                                placeholder="Search for any agent service.."/>
                             <Button className="mr-3 h-full"><Search className="-mx-1"/></Button>
                         </div>
                     </div>
                     <Image className="w-fit pr-2" src={right} alt="AI character images"/>
-                    
-                </section>
+                
+                </section>}
+                
+                
+            {/*    todo add mobile handler*/}
             </div>
             
             {/* Featured Agents */}
