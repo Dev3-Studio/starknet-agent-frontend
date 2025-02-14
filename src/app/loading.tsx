f7ed034eb8cfb8f2ac21476711c2d0a8dcc4be86
ColourfulMelon <jordynnitch@gmail.com>
2025-02-15 00:43:19 +0200

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loading() {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <p className="text-5xl">Loading...</p>
+            
+        </div>
+    );
+}
