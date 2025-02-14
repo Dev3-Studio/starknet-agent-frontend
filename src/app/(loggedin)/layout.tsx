8a6f443e673323bd30bbc9f4dcd54f924d15000b
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-02-14 02:40:00 +0200

    feat: Protect route from users that are not logged in

--- src/app/(loggedin)/layout.tsx
+++ src/app/(loggedin)/layout.tsx
@@ -1,11 +1,16 @@
 import { AppSidebar } from '@/app-sidebar';
 import FixedSidebar from '@/FixedSidebar';
+import { auth } from '@/config/auth';
+import { redirect } from 'next/navigation';
 
-export default function ChatLayout({
+export default async function ChatLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await auth();
+    if (!session) redirect('/?error=unauthorized');
+    
     return (
         <div className="relative w-full">
             <div className="fixed top-0 left-0 h-full">
