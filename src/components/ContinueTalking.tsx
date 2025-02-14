fafbea137107f007416082b2c5cd07e2ac303cfe
ColourfulMelon <jordynnitch@gmail.com>
2025-02-14 17:57:03 +0200

    add sharp

---
+++ src/components/ContinueTalking.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Card } from '@/ui/card';
+import { AgentPublic } from '@/lib/dto';
+import Image from 'next/image';
+import { formatAddress } from '@/lib/utils';
+import { MessageSquare } from 'lucide-react';
+import ChatInput from '@/ChatInput';
+
+export default function ContinueTalking(props: AgentPublic){
+    
+    // todo finish implement
+    return (
+        <Card>
+            {/*ai info section*/}
+            <div className="flex h-20 rounded-3xl overflow-hidden p-1">
+                <Image
+                    src={props.image}
+                    alt="Agent Image"
+                    width={1000}
+                    height={1000}
+                    className="rounded-2xl"
+                />
+                
+                <div>
+                    <h2>{props.name}</h2>
+                    
+                    <div className="flex text-muted-foreground">
+                        
+                        <p>By {formatAddress(props.creator.walletAddress)}</p>
+                        
+                        <div className="flex">
+                            <MessageSquare className="size-4 my-auto mr-1" />
+                            {props.totalMessages}
+                        </div>
+                    
+                    </div>
+                    
+                </div>
+            </div>
+            
+            {/*chat section*/}
+            <div className="bg-muted h-60 grid grid-rows-[1fr,auto]">
+                
+                <div className="overflow-y-scroll">
+                
+                </div>
+            
+                {/*message box*/}
+                {/*<ChatInput inputRef={inputRef} onSend={sendMessage}/>*/}
+                
+            </div>
+        
+        </Card>
+    )
+}
